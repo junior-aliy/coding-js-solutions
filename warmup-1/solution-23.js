53d51c6f8534536a602b0bb1cb133798e0b33837
Junior Aliy <junioraliy0706@gmail.com>
2024-10-22 20:51:36 +0500

    Update solution-23.2

--- warmup-1/solution-23.js
+++ warmup-1/solution-23.js
@@ -1,3 +1,8 @@
-const foo => () {
-  console.log("foo function has been start!")
+function startOz(str){
+  let result = ""
+  if(str[0] == "o") result = result + str[0]
+  if(str[1] == "z") result = result + str[1]
+  return result
 }
+
+startOz("oDev team")
